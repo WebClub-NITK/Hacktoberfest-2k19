0909f8106e678f07b892ab2dec4c709c3de0d58b
Ikjot Singh Dhody <ikjotsd@gmail.com>
2019-10-02 23:37:36 +0530

    Edit for previously submitted erroneous program

--- algorithms/56/k_largest.c
+++ algorithms/56/k_largest.c
@@ -2,29 +2,24 @@
 
 int main()
 {
-    printf("Please enter the array size:\n");
-    int size;
-    scanf("%d",&size);
-    int arr[size];
-    int max=0;
-    for(int i=0;i<size;i++)     //creating the array
-    {scanf("%d",&arr[i]);
-        if(arr[i]>max)max=arr[i];
+    printf ("Enter number of elements : ");
+    long n; //stores size of array
+    scanf ("%ld",&n);
+    long array [n];
+    int max = 0;
+    printf("Enter array elements : ");
+    for(long i=0; i<n; i++) {
+        scanf("%ld",&array[i]); //input for array[]
+        if (array[i] > max) max = array[i];
     }
-    int newarr[max+1];
-    for(int i=0;i<size;i++)
-    {
-        newarr[arr[i]]+=1;
-    }
-    printf("Enter k\n");
-    int k=0;
-    scanf("%d",&k);
-    int temp=k;
-    int i=0;
-    while(temp>0)
-    {temp=temp-newarr[i];
-        i++;
-    }
-    int answer=i;
-    printf("The kth largest element in given array is : %d\n",answer);
-}//done in linear time
+    long count[max+1];
+    for (int i=0; i<max+1; i++) count[i]=0;
+    for(long i=0; i<n; i++) count[array[i]]++; //updating count[] array to hold repetitions of array[] elements
+    printf ("Enter value of k : ");
+    long k;
+    scanf("%ld",&k);
+    long i = max+1;
+    while (k>0 && i>0) k -= count[--i];
+    long answer = i;
+    printf("The kth largest element is : %ld\n",answer);
+}
