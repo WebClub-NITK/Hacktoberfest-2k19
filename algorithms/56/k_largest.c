479c8de9c7e1db70a029a8827d91b68946b8cf5a
bigBrain1901 <asishaan.191cs124@nitk.edu.in>
2019-10-02 23:39:57 +0530

    issue 56 part 1

---
+++ algorithms/56/k_largest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    //input section
+    printf ("Enter number of elements : ");
+    long n; //stores size of array
+    scanf ("%ld",&n);
+    long array [n];
+    int max = 0;
+    printf("Enter array elements : ");
+    for(long i=0; i<n; i++) {
+        scanf("%ld",&array[i]); //input for array[]
+        if (array[i] > max) max = array[i];
+    }
+
+    //processing section
+    long count[max+1];
+    for (int i=0; i<max+1; i++) count[i]=0;
+    for(long i=0; i<n; i++) count[array[i]]++; //updating count[] array to hold repetitions of array[] elements
+    printf ("Enter value of k : ");
+    long k;
+    scanf("%ld",&k);
+    long i = max+1;
+    while (k>0 && i>0) k -= count[--i];
+    long answer = i;
+    
+    //output section
+    printf("The kth largest element is : %ld\n",answer);
+}
