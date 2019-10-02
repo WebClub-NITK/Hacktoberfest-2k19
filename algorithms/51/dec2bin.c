30983d1c5ce0750d36c754446987889c28a2f889
BlazkowiczTon <nithinsampige2018@gmail.com>
2019-10-03 00:38:28 +0530

    decimal to binary code in C

---
+++ algorithms/51/dec2bin.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main (void){
+
+    int number;
+
+    int i=0,j=0;
+
+    int binary[100];
+
+    //INPUT.
+    scanf("%d",&number);
+
+    //assigning binary.
+    while (number>0){
+        binary[i]=number%2;
+        number=number/2;
+        i=i+1;
+    }
+    //printing binary from reverse.
+    for (j=i-1;j>=0;j--){
+        printf("%d",binary[j]);
+
+    }
+}
