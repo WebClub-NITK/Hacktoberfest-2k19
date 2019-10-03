4a9211aaece3c1027390d32f820628127487dd79
adityagamer786 <adityagamers786@gmail.com>
2019-10-03 12:54:30 +0530

    Added 1 entry to DB

--- systems/63-DatabaseIntegration/nitk/student/urls.py
+++ systems/63-DatabaseIntegration/nitk/student/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
-  path("/register", views.register, name="register")
+  path("register", views.register, name="register")
 ]
