6ac70c3ba5f3a4a9d6183819ee06d8f87e4dbc21
adityagamer786 <adityagamers786@gmail.com>
2019-10-03 08:18:39 +0530

    Created model Student

--- systems/63-DatabaseIntegration/nitk/student/models.py
+++ systems/63-DatabaseIntegration/nitk/student/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length = 64)
+    last_name = models.CharField(max_length = 64)
+    fathers_name = models.CharField(max_length = 64)
+    AIR = models.IntegerField()
+    course = models.CharField(max_length = 64)
