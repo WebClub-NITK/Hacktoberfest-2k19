2e0e343133eaa811e498aa2c785d6a94f7f2ecff
Abraham Francis <abfr049@gmail.com>
2019-10-03 23:05:32 +0530

    improve IPv4 verification

--- Issue 166 - IPv4 Class/C.c
+++ Issue 166 - IPv4 Class/C.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int valid_number(char *num)
+{
+    while (*num) {
+	if (*num < '0' || *num > '9') return 0;
+	++num;
+    }
+    return 1;
+}
+
+int valid_ipv4_address(char *addr)
+{
+    char *split = strtok(addr, ".");
+    int dots = -1;
+    long long oct;
+
+    while (split != NULL) {
+	++dots;
+
+	if (!valid_number(split)) return 0;
+	
+	oct = atoll(split);
+	if (oct < 0 || oct > 255) return 0;
+
+	split = strtok(NULL, ".");
+    }
+
+    return (dots == 3);
+}
 
 char ipv4_class(char *addr)
 {
@@ -7,11 +38,7 @@ char ipv4_class(char *addr)
     sscanf(addr, "%d.%d.%d.%d", &oct1, &oct2, &oct3, &oct4);
 
 /*  If oct1 id invalid or 127 (reserved for loopback addresses), return NULL */
-    if ( oct1 < 1 || oct1 == 127 || oct1 > 255
-      || oct2 > 255
-      || oct3 > 255
-      || oct4 > 255 )
-    return 0;
+    if (oct1 < 1 || oct1 == 127 || oct1 > 255) return 0;
 
     if (oct1 <= 126) return 'A';
     if (oct1 <= 191) return 'B';
@@ -22,12 +49,12 @@ char ipv4_class(char *addr)
 
 int main(void)
 {
-    char address[16];
+    char address[1024];
     printf("Enter the IPv4 address: ");
-    scanf("%15s", address);
+    scanf("%1023s", address);
     char class = ipv4_class(address);
-    if (class) /* If class is valid */
-	printf("This is a class %c IPv4 address.\n", ipv4_class(address));
+    if (valid_ipv4_address(address) && class) /* If class is valid */
+	printf("This is a class %c IPv4 address.\n", class);
     else
 	printf("This is an invalid IPv4 address.\n");
     return 0;
