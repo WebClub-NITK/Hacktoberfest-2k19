9b78b7c3815f0ba943e186a6b3e7e90e528136fc
kuljeetsingh <kuljeetsingh951@gmail.com>
2019-10-03 12:10:43 +0530

    Responsive Login page using html,css and javascript

---
+++ development/Login_page/js/login.js
@@ -0,0 +1,22 @@
+var attempt = 5; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 5 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
