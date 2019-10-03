bf2e5e2ba8b1e505edd8787cf800986d4d3b03d1
Antoine He <antoine.he@protonmail.com>
2019-10-03 20:33:06 +0200

    Reformat code_factoring.c
    This commit reformats code_factoring.c so it adheres to good
    programming practices.

--- algorithms/Recursion/code_refactoring.c
+++ algorithms/Recursion/code_refactoring.c
@@ -1,12 +1,25 @@
-// Program
+// C program to convert a number from base 10 to base 2
+
 #include <stdio.h>
-void c(int);
-int main(void){
-int n;
-scanf("%d",&n);
-c(n);}
-void c(int n){
-int r=n%2;
-if(n==0) return; c(n/2);
-printf("%d", r);}
-// End of program
+
+void decimal_to_binary(int);
+
+// Driver code
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    decimal_to_binary(n);
+}
+
+// Converts a number from base 10 to base 2
+void decimal_to_binary(int n)
+{
+    int r = n % 2;
+    if (n == 0)
+    {
+        return;
+    }
+    decimal_to_binary(n / 2);
+    printf("%d", r);
+}
