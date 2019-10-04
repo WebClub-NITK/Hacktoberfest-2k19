265d3fa79781b8a5af7bb346c1705a9d45e96c84
tharun634 <53267275+tharun634@users.noreply.github.com>
2019-10-04 09:23:29 +0530

    Created LoginScript.py

---
+++ systems/sophosLogin/loginScript.py
@@ -0,0 +1,9 @@
+import requests
+
+def login():
+            login_url = "https://nac.nitk.ac.in:8090/login.xml"	
+            r = requests.post(login_url, data={'mode': 191, 'username': 'username', 'password': 'password'}
+            pass
+
+if __name__ == '__main__':
+	login()
