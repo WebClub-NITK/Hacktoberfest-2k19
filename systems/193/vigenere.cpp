61bee74b93a9dd371c32e82b0db1b070c5ef1b84
him1anshu <himanshu01011997@gmail.com>
2019-10-04 18:11:55 +0530

    Make the specific changes in code and have done indentation

--- systems/193/vigenere.cpp
+++ systems/193/vigenere.cpp
@@ -1,78 +1,64 @@
 #include<iostream>
+
 #include<string.h>
+
 using namespace std;
-int main()
-{
+int main() {
 
-int i,keylen,j=0;
-char en,dec,ch;
-string text,key,deci,enci;
+  int i, keylen, j = 0;
+  char en, dec, ch;
+  string text, key, deci, enci;
 
-cout<<"Enter the key used for encryption : \n";
-cin>>key;
+  cout << "Enter the key used for encryption : \n";
+  cin >> key;
 
-cin.ignore();
+  cin.ignore();
 
-cout<<"Enter the text for encryption : \n";
-getline(cin,text);
+  cout << "Enter the text for encryption : \n";
+  getline(cin, text);
 
-for(i=0;text[i]!='\0';i++)
-{
-if(isalpha(text[i]))
-{
-text[j]=text[i];
-j++;
+  keylen = key.length();
+
+  for (i = 0; text[i] != '\0'; i++) {
+
+    if (isupper(text[i])) {
+      ch = text[i] % 65;
+      en = ((ch + (toupper(key[j % keylen]) % 65)) % 26) + 65;
+      text[i] = en;
+      j++;
+    }
+
+    if (islower(text[i])) {
+      ch = text[i] % 97;
+      en = ((ch + (tolower(key[j % keylen]) % 97)) % 26) + 97;
+      text[i] = en;
+      j++;
+    }
+
+  }
+
+  cout << "Encrypted Text : " << text << "\n";
+
+  j = 0;
+
+  for (i = 0; text[i] != '\0'; i++) {
+    if (isupper(text[i])) {
+      ch = text[i] % 65;
+      dec = ((26 + (ch - (toupper(key[j % keylen])) % 65)) % 26) + 65;
+      text[i] = dec;
+      j++;
+    }
+
+    if (islower(text[i])) {
+      ch = text[i] % 97;
+      dec = ((26 + (ch - (tolower(key[j % keylen])) % 97)) % 26) + 97;
+      text[i] = dec;
+      j++;
+    }
+
+  }
+
+  cout << "Decrypted Text : " << text << "\n";
+
+  return 0;
 }
-}
-text[j]='\0';
-
-text.erase(j);
-
-keylen=key.length();
-
-for(i=0;text[i]!='\0';i++)
-{
-
-if(isupper(text[i]))
-{
-ch=text[i]%65;
-en=((ch+(toupper(key[i%keylen])%65))%26)+65;
-text[i]=en;
-}
-
-if(islower(text[i]))
-{
-ch=text[i]%97;
-en=((ch+(tolower(key[i%keylen])%97))%26)+97;
-text[i]=en;
-}
-
-}
-
-cout<<"Encrypted Text : "<<text<<"\n";
-
-for(i=0;text[i]!='\0';i++)
-{
-if(isupper(text[i]))
-{
-ch=text[i]%65;
-dec=((26+(ch-(toupper(key[i%keylen]))%65))%26)+65;
-text[i]=dec;
-}
-
-if(islower(text[i]))
-{
-ch=text[i]%97;
-dec=((26+(ch-(tolower(key[i%keylen]))%97))%26)+97;
-text[i]=dec;
-}
-
-}
-
-cout<<"Decrypted Text : "<<text<<"\n";
-
-return 0;
-}
-
-
-
