5020ee3122f09186c4f96c4f24a20bd8ced2115c
Himanshu307 <himanshukumar.191cs122@nitk.edu.in>
2019-10-05 16:33:38 +0530

    swapping

--- algorithms/Swap_with_max/SOURCE_FILE.cpp
+++ algorithms/Swap_with_max/SOURCE_FILE.cpp
@@ -1,45 +1,50 @@
 #include<stdio.h>
-//MAXIMUM FUNCTION
-void maximum(int arr)
-{ int max;
- for(int i=0;i<ind;i++)
- {
-  if(max<arr[i])
-  max=arr[i];
- }	
- return max;
-}
-//SWAP FUNCTION
-void swap(int arr[],int ind)
-{
-	int l=maximum(arr);
-	int temp;
-	temp=arr[l];
-	arr[l]=arr[ind];
-	arr[ind]=temp;
-}
+
+int ind,N;
 
 //MAIN FUNCTION
 int main()
-{   int N;
+{   int i=0;
+    long int arr[100];
 	printf("ENTER THE NUMBER OF ELEMENTS IN THE ARRAY:");
 	scanf("%d",&N);
-	int i=0;
-	int arr[N];
 	for(i=0;i<N;i++)
 	{
-		scanf("%d",arr+i);
+		scanf("%ld",&arr[i]);
 	}
-	int ind;
 	printf("ENTER THE INDEX TO SWAP WITH THE MAXIMUM:");
 	scanf("%d",&ind);
-	swap(arr,ind);
-	int j=0;
+
+	Swap(arr,ind);
+	return 0;
+}
+
+//MAXIMUM FUNCTION
+long int maximum(long int arr[])
+{ 
+	long int max=0;
+	for(int i=0;i<N;i++)
+	{
+		if(max<arr[i])
+		max=arr[i];
+	}	
+ 	return max;
+}
+
+//SWAP FUNCTION
+int Swap(long int arr[],int ind)
+{
+	long int l;
+	l=maximum(arr);
+	arr[ind]=arr[ind]+l;
+	l=arr[ind]-l;
+	arr[ind]=arr[ind]-l;
+        int j=0;
+
 	//DISPLAYING THE ARRAY
 	for(j=0;j<N;j++)
 	{
-		printf("%d ",arr[i]);
+		printf("%ld ",arr[j]);
 	}
-	
-	
+	return 0;
 }
