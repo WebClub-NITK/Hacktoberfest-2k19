8762327ff1974ad62a368030822fedb3a868b960
tharun634 <53267275+tharun634@users.noreply.github.com>
2019-10-08 18:32:44 +0530

    Updated LoginScript.py

--- systems/sophosLogin/loginScript.py
+++ systems/sophosLogin/loginScript.py
@@ -2,8 +2,8 @@ import requests
 
 def login():
             login_url = "https://nac.nitk.ac.in:8090/login.xml"	
-            r = requests.post(login_url, data={'mode': 191, 'username': 'username', 'password': 'password'}
-            pass
+            r = requests.post(login_url, data={'mode': 191, 'username': 'username', 'password': 'password'})
+            
 
 if __name__ == '__main__':
 	login()
